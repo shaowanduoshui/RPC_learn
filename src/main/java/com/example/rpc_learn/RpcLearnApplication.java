1a8038c6396b8aa757e301b31d9882f7c64b4267
yancy.wu <yancy.wu@yunzhihui.com>
2021-07-17 12:31:51 +0800

    first

---
+++ src/main/java/com/example/rpc_learn/RpcLearnApplication.java
@@ -0,0 +1,13 @@
+package com.example.rpc_learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpcLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RpcLearnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rpc_learn/RpcLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rpc_learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpcLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
